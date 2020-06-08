ec30499b3f922475ff6c6a3b8f5a93e3350fa9e6
Mark Windholtz <mark@agiledna.com>
2020-06-08 15:32:37 -0400

    fixed test

--- test/ddd_ex_tickets/warehouse/venue_test.exs
+++ test/ddd_ex_tickets/warehouse/venue_test.exs
@@ -1,3 +1,12 @@
 defmodule DddExTickets.Warehouse.VenueTest do
   use DddExTickets.DomainCase
+
+  alias DddExTickets.Warehouse.Venue
+
+  test "handle_call(:reserve_seat" do
+    available = [1, 2, 3]
+    state = %Venue{available: available}
+
+    {:reply, :ok, state} = Venue.handle_call(:reserve_seat, self(), state)
+  end
 end
