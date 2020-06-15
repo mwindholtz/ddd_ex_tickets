50d610e59d7bbdefcc86dd123b3ae59610d0a51e
Mark Windholtz <mark@agiledna.com>
2020-06-15 14:07:34 -0400

    adds scoping to  tests

--- test/ddd_ex_tickets_web/live/page_live_test.exs
+++ test/ddd_ex_tickets_web/live/page_live_test.exs
@@ -4,29 +4,33 @@ defmodule DddExTicketsWeb.PageLiveTest do
   import Phoenix.LiveViewTest
   alias DddExTicketsWeb.PageLive
   alias DddExTickets.DomainEvent
+  alias DddExTickets.Warehouse.Venue
 
-  @venue_module DddExTickets.Warehouse.Venue
-  setup do
-    # Since Venue is a global resource
-    # we need to stop it between each test
-    # The supervisor will restart it, up to a point
-    GenServer.stop(@venue_module)
-    wait_for_venue_to_restart()
-    :ok
-  end
+  describe "using global servers" do
+    setup do
+      # Since Venue is a global resource
+      # we need to stop/start it after each test
+      # The supervisor will restart it, up to a point
+      on_exit(fn ->
+        GenServer.stop(Venue)
+        wait_for_venue_to_restart()
+        :ok
+      end)
+    end
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
+    test "disconnected and connected render", %{conn: conn} do
+      {:ok, page_live, disconnected_html} = live(conn, "/")
 
-    assert disconnected_html =~ "Welcome to DddExTickets!"
-    assert render(page_live) =~ "Welcome to DddExTickets!"
-    assert render(page_live) =~ "Remaining: 30"
-  end
+      assert disconnected_html =~ "Welcome to DddExTickets!"
+      assert render(page_live) =~ "Welcome to DddExTickets!"
+      assert render(page_live) =~ "Remaining: 30"
+    end
 
-  test "reserve a ticket and see 29 remaining", %{conn: conn} do
-    DddExTickets.Warehouse.Venue.reserve_seat()
-    {:ok, page_live, _disconnected_html} = live(conn, "/")
-    assert render(page_live) =~ "Remaining: 29"
+    test "reserve a ticket and see 29 remaining", %{conn: conn} do
+      DddExTickets.Warehouse.Venue.reserve_seat()
+      {:ok, page_live, _disconnected_html} = live(conn, "/")
+      assert render(page_live) =~ "Remaining: 29"
+    end
   end
 
   test "handle_info(%DomainEvent{name: :seat_released}" do
@@ -38,17 +42,16 @@ defmodule DddExTicketsWeb.PageLiveTest do
     assert socket.assigns == %{remaining_count: 30, results: []}
   end
 
-  # test "handle_info(%DomainEvent{name: :seat_reserved}" do
-  #   socket = %Phoenix.LiveView.Socket{}
-  #   domain_event = DomainEvent.seat_reserved(1)
+  test "handle_info(%DomainEvent{name: :seat_reserved}" do
+    socket = %Phoenix.LiveView.Socket{}
+    domain_event = DomainEvent.seat_reserved(1)
 
-  #   assert {:noreply, socket} = PageLive.handle_info(domain_event, socket)
-
-  #   assert socket.assigns == %{remaining_count: 30, results: []}
-  # end
+    assert {:noreply, socket} = PageLive.handle_info(domain_event, socket)
+    assert socket.assigns == %{remaining_count: 30, results: []}
+  end
 
   defp wait_for_venue_to_restart do
-    case Process.whereis(@venue_module) do
+    case Process.whereis(Venue) do
       nil ->
         Process.sleep(10)
         wait_for_venue_to_restart()
